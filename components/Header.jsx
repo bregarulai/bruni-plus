a5aa80a8aece49d75957d61555dfd22790d3ede7
bregarulai <bregarulai@gmail.com>
2021-12-19 15:58:17 -0500

    added logo to header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,13 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='sticky bg-[#040714] top-0 z-[1000] h-[4.5rem] flex items-center px-10 md:px-12'>
+      {/* <div>Bruni+</div> */}
+      <Image src='/images/logo.svg' width={80} height={80} />
+    </header>
+  );
 };
 
 export default Header;
