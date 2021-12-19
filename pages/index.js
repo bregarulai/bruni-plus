59311fbe372d4818a2aa5a45735b5f4e4dd0c909
bregarulai <bregarulai@gmail.com>
2021-12-19 15:21:48 -0500

    created components, images and videos folder and added needed images and videos

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+    <div className=''>
       <Head>
         <title>Bruni Plus</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'></main>
+      <main className=''></main>
     </div>
   );
 }
