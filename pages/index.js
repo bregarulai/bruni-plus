631bd1295442cdef8d47cc39c92e04aa4b10bf8f
bregarulai <bregarulai@gmail.com>
2021-12-19 19:02:40 -0500

    getting session in server side

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import { getSession } from 'next-auth/client';
 import Head from 'next/head';
+
 import { Header } from '../components';
 
 export default function Home() {
@@ -12,4 +14,14 @@ export default function Home() {
       <main className=''></main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
