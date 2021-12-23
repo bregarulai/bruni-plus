cfe42a63e99f5585c0f621e2c83c920e60009dd7
bregarulai <bregarulai@gmail.com>
2021-12-23 16:46:16 -0500

    added video on hover

--- components/Brands.jsx
+++ components/Brands.jsx
@@ -6,6 +6,14 @@ const Brands = () => {
     <section className='flex flex-col md:flex-row items-center justify-center mt-10 gap-6 px-8 max-w-[1400px] mx-auto'>
       <div className='brand group'>
         <Image src='/images/disnep.png' layout='fill' objectFit='cover' />
+        <video
+          autoPlay
+          loop
+          playsInline
+          className='hidden group-hover:inline rounded-lg object-cover transform hover:scale-105 transition duration-300'
+        >
+          <source src='/videos/disney.mp4' type='video/mp4' />
+        </video>
       </div>
     </section>
   );
