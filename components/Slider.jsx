76c24f589a76b4a99cede4068be5e2b8c44083d0
bregarulai <bregarulai@gmail.com>
2021-12-23 16:07:37 -0500

    finished slider component

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 
 const Slider = () => {
-  return <div>Slider</div>;
+  return (
+    <section className='relative mt-7 shadow-2xl max-w-screen-2xl mx-auto'>
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div>
+          <img loading='lazy' src='/images/slider-1.jpg' alt='slider' />
+        </div>
+        <div>
+          <img loading='lazy' src='/images/slider-2.jpg' alt='slider' />
+        </div>
+        <div>
+          <img loading='lazy' src='/images/slider-3.jpg' alt='slider' />
+        </div>
+        <div>
+          <img loading='lazy' src='/images/slider-4.jpeg' alt='slider' />
+        </div>
+      </Carousel>
+    </section>
+  );
 };
 
 export default Slider;
