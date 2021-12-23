c76bd8076d68e55a81189d5238d910f7cc1ac8ac
bregarulai <bregarulai@gmail.com>
2021-12-23 16:53:37 -0500

    finished brands component

--- components/Brands.jsx
+++ components/Brands.jsx
@@ -15,6 +15,54 @@ const Brands = () => {
           <source src='/videos/disney.mp4' type='video/mp4' />
         </video>
       </div>
+      <div className='brand group'>
+        <Image src='/images/pixar.png' layout='fill' objectFit='cover' />
+        <video
+          autoPlay
+          loop
+          playsInline
+          className='hidden group-hover:inline rounded-lg object-cover transform hover:scale-105 transition duration-300'
+        >
+          <source src='/videos/pixar.mp4' type='video/mp4' />
+        </video>
+      </div>
+      <div className='brand group'>
+        <Image src='/images/marvel.png' layout='fill' objectFit='cover' />
+        <video
+          autoPlay
+          loop
+          playsInline
+          className='hidden group-hover:inline rounded-lg object-cover transform hover:scale-105 transition duration-300'
+        >
+          <source src='/videos/marvel.mp4' type='video/mp4' />
+        </video>
+      </div>
+      <div className='brand group'>
+        <Image src='/images/starwars.png' layout='fill' objectFit='cover' />
+        <video
+          autoPlay
+          loop
+          playsInline
+          className='hidden group-hover:inline rounded-lg object-cover transform hover:scale-105 transition duration-300'
+        >
+          <source src='/videos/star-wars.mp4' type='video/mp4' />
+        </video>
+      </div>
+      <div className='brand group'>
+        <Image
+          src='/images/national-geographic.png'
+          layout='fill'
+          objectFit='cover'
+        />
+        <video
+          autoPlay
+          loop
+          playsInline
+          className='hidden group-hover:inline rounded-lg object-cover transform hover:scale-105 transition duration-300'
+        >
+          <source src='/videos/national-geographic.mp4' type='video/mp4' />
+        </video>
+      </div>
     </section>
   );
 };
