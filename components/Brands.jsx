38a4f8ab4d3dbaff917f2cf370b333a13840170c
bregarulai <bregarulai@gmail.com>
2021-12-23 16:21:52 -0500

    created section container and added first image

--- components/Brands.jsx
+++ components/Brands.jsx
@@ -1,7 +1,14 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Brands = () => {
-  return <div>Brands</div>;
+  return (
+    <section className='flex flex-col md:flex-row items-center justify-center mt-10 gap-6 px-8 max-w-[1400px] mx-auto'>
+      <div className='brand group'>
+        <Image src='/images/disnep.png' layout='fill' objectFit='cover' />
+      </div>
+    </section>
+  );
 };
 
 export default Brands;
