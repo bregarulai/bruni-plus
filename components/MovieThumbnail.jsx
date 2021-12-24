dadd5ca8fde5ce4de0e447c1e71aa878f75a9c8e
bregarulai <bregarulai@gmail.com>
2021-12-24 12:06:30 -0500

    finished movie thumbnail component

--- components/MovieThumbnail.jsx
+++ components/MovieThumbnail.jsx
@@ -6,7 +6,10 @@ const MovieThumbnail = ({ result }) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original/';
   const router = useRouter();
   return (
-    <article>
+    <article
+      className='flex min-w-[250px] min-h-[270px] md:min-w-[330px] md:min-h-[210px] rounded-lg overflow-hidden shadow-xl cursor-pointer border-[3px] border-[#f9f9f9] border-opacity-10 hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300'
+      onClick={() => router.push(`/movie/${result.id}`)}
+    >
       <Image
         src={
           `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
--- components/MoviesCollection.jsx
+++ components/MoviesCollection.jsx
@@ -5,9 +5,11 @@ const MoviesCollection = ({ title, results }) => {
   return (
     <section className='relative flex flex-col space-y-2 my-10 px-8 max-w-[1400px] mx-auto'>
       <h2 className='font-semibold'>{title}</h2>
-      {results.map((result) => (
-        <MovieThumbnail key={result.id} result={result} />
-      ))}
+      <div className='flex space-x-6 overflow-hidden overflow-x-scroll scrollbar-hide p-2 -m-2'>
+        {results.map((result) => (
+          <MovieThumbnail key={result.id} result={result} />
+        ))}
+      </div>
     </section>
   );
 };
