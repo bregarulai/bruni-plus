81e6a08b915ceb7d8aca6cfe1c900f4684639da4
bregarulai <bregarulai@gmail.com>
2021-12-23 21:13:17 -0500

    fetching data from api

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { getSession, useSession } from 'next-auth/client';
 import Head from 'next/head';
 
@@ -10,8 +11,14 @@ import {
   Slider,
 } from '../components';
 
-export default function Home() {
+export default function Home({
+  popularMovies,
+  popularShows,
+  topRatedMovies,
+  topRatedShows,
+}) {
   const [session] = useSession();
+
   return (
     <div className=''>
       <Head>
@@ -36,10 +43,43 @@ export default function Home() {
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
+  const [
+    popularMoviesRes,
+    popularShowsRes,
+    topRatedMoviesRes,
+    topRatedShowsRes,
+  ] = await Promise.all([
+    axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
+    ),
+    axios.get(
+      `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
+    ),
+    axios.get(
+      `
+https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`
+    ),
+    axios.get(
+      `
+https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`
+    ),
+  ]);
+
+  const [popularMovies, popularShows, topRatedMovies, topRatedShows] =
+    await Promise.all([
+      popularMoviesRes.data,
+      popularShowsRes.data,
+      topRatedMoviesRes.data,
+      topRatedShowsRes.data,
+    ]);
 
   return {
     props: {
       session,
+      popularMovies,
+      popularShows,
+      topRatedMovies,
+      topRatedShows,
     },
   };
 }
