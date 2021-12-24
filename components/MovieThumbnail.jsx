793e5415b9d102dc79a180a96e34eba0d4ba30eb
bregarulai <bregarulai@gmail.com>
2021-12-24 11:56:50 -0500

    added image to thumbnail component

--- components/MovieThumbnail.jsx
+++ components/MovieThumbnail.jsx
@@ -1,9 +1,23 @@
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const MovieThumbnail = ({ result }) => {
+  const BASE_URL = 'https://image.tmdb.org/t/p/original/';
+  const router = useRouter();
   return (
     <article>
-      <span>{result.title}</span>
+      <Image
+        src={
+          `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+          `${BASE_URL}${result.poster_path}`
+        }
+        width={330}
+        height={210}
+        objectFit='cover'
+        className='rounded-lg'
+        alt={`${result.title} poster`}
+      />
     </article>
   );
 };
