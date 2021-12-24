040559bf962ad7b5334019133f106f05d16b1bca
bregarulai <bregarulai@gmail.com>
2021-12-24 14:36:46 -0500

    added head and header component to movie detail page

--- pages/movie/[id].jsx
+++ pages/movie/[id].jsx
@@ -1,14 +1,25 @@
 import axios from 'axios';
+import { getSession } from 'next-auth/client';
+import Head from 'next/head';
 import React from 'react';
+import { Header } from '../../components';
 
 const MovieDetail = ({ result }) => {
-  console.log({ result });
-  return <div>Movie Detail</div>;
+  return (
+    <>
+      <Head>
+        <title>{result.title || result.original_name}</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Header />
+    </>
+  );
 };
 
 export default MovieDetail;
 
 export async function getServerSideProps(context) {
+  const session = await getSession(context);
   const { id } = context.query;
   const res = await axios.get(
     `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&language=en-US&append_to_response=videos`
@@ -18,6 +29,7 @@ export async function getServerSideProps(context) {
   return {
     props: {
       result,
+      session,
     },
   };
 }
