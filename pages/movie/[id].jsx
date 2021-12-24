2e0d1ee621d9478696c1926f403b5b458f1d68d3
bregarulai <bregarulai@gmail.com>
2021-12-24 14:27:44 -0500

    fetching movie details from api

--- pages/movie/[id].jsx
+++ pages/movie/[id].jsx
@@ -1,7 +1,23 @@
+import axios from 'axios';
 import React from 'react';
 
-const MovieDetail = () => {
+const MovieDetail = ({ result }) => {
+  console.log({ result });
   return <div>Movie Detail</div>;
 };
 
 export default MovieDetail;
+
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+  const res = await axios.get(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&language=en-US&append_to_response=videos`
+  );
+  const result = await res.data;
+
+  return {
+    props: {
+      result,
+    },
+  };
+}
